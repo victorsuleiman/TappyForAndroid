85eb3b5ad9f0a9231940d0415267a47aabb1efd6
JamesDaiD <65271451+JamesDaiD@users.noreply.github.com>
2020-10-12 07:15:03 -0700

    Added a button
    Just playing around, nothing important yet

---
+++ SampleProject/app/src/main/java/com/example/sampleproject/Level1.java
@@ -0,0 +1,34 @@
+package com.example.sampleproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Level1 extends AppCompatActivity {
+
+    int i = 0;
+    Button tapBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level1);
+
+        tapBtn = findViewById(R.id.tappyTappy);
+
+        tapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i++;
+                String tappy = (10 - i) + " more!";
+                tapBtn.setText(tappy);
+                if (i>= 9)
+                {
+                    startActivity(new Intent(Level1.this, Level2.class));
+                }
+            }
+        });
+    }
+}
--- SampleProject/app/src/main/java/com/example/sampleproject/MainActivity.java
+++ SampleProject/app/src/main/java/com/example/sampleproject/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.sampleproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.logging.Level;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    Button startGameBtn; //declare button object
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startGameBtn = findViewById(R.id.startButton); //hook button object to preferred button
+
+        /*give the button an onClickListener*/
+        startGameBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, Level1.class));
+            }
+        }); //end setOnClickListener of startGameBtn
+
     }
 }
