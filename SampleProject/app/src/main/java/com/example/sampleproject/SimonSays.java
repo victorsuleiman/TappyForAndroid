2be8d08123dda9f40f857979b1a2c4789881ae28
czzbandicoot <victorsuleiman.1994@gmail.com>
2020-11-03 15:19:23 -0800

    Simon: played with some logic, added the layout again D=

--- SampleProject/app/src/main/java/com/example/sampleproject/SimonSays.java
+++ SampleProject/app/src/main/java/com/example/sampleproject/SimonSays.java
@@ -3,6 +3,10 @@ package com.example.sampleproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SimonSays extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class SimonSays extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simon_says);
+
+        Button testButton = findViewById(R.id.buttonTest);
+
+        final Button greenButton = findViewById(R.id.buttonGreen);
+
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SimonSays.this,"Green button pressed",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        greenButton.performClick();
+
+                    }
+                }, 500); // 1000 mili seconds is 1 second
+            }
+        });
+
     }
 }
