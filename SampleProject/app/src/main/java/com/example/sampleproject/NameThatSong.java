a74f3383385f80bfc20f0aea4e86fece36917ac7
JamesDaiD <65271451+JamesDaiD@users.noreply.github.com>
2020-11-16 02:25:06 -0800

    Finishing Name That Song
    Get Random Song everytime
    Timer implemented
    All data read from file, can easily be converted to DB

--- SampleProject/app/src/main/java/com/example/sampleproject/NameThatSong.java
+++ SampleProject/app/src/main/java/com/example/sampleproject/NameThatSong.java
@@ -18,7 +18,19 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
+/*
+A file with data about all songs is read, this data will be parsed and put inside objects
+These objects are stored inside a List<Song>
+When the activity start, a random Song is selected
+A timer starts running the moment the user hear the song (a boolean keeps track of whether the user have heard the song or not)
+The user enter their guess into the editText. This will then be compared with the song name stored inside the object
+User guesses right => Get the time they needed to guess, reset timer, get a new song
+User guesses wrong => Display hints (this can happen a few times). Hints includes album art, artist name and trivia
+
+The game can be repeated, the timer and song generator will work properly
+ */
 public class NameThatSong extends AppCompatActivity {
 
     Button submitBtn;
@@ -30,11 +42,11 @@ public class NameThatSong extends AppCompatActivity {
     ImageButton playPauseBtn;
     long startTime; //used to calculate time taken for guess
     long endTime;
+    long timeTaken;
 
-    //TODO: get random current song
-    int currentSong = 0; //current song that's being played
     boolean songPlayed = false; //check if song has been played or not
-
+    int currentSong; //holds the song currently being played
+    Random rand = new Random(); //used to generate random number for a random song
 
 
     @Override
@@ -47,6 +59,10 @@ public class NameThatSong extends AppCompatActivity {
         playPauseBtn = findViewById(R.id.playPauseBtn);
 
         songList = SongParser(); //getting song list
+
+        currentSong = rand.nextInt(songList.size()); //get random current song
+
+        Toast.makeText(this, songList.get(currentSong).toString(), Toast.LENGTH_SHORT).show();
 
         /*Clicking Play/pause button*/
         playPauseBtn.setOnClickListener(new View.OnClickListener()
@@ -65,9 +81,10 @@ public class NameThatSong extends AppCompatActivity {
                 {
                     if (songPlayed == false) //song never played => starts timer and marks that the user has listened
                     {
-                        startTime=System.currentTimeMillis(); //starts timer
+                        startTime = System.currentTimeMillis(); //starts timer once the user have heard the song
                         songPlayed = true;
                     }
+
                     /*play song*/
                     aPlayer = MediaPlayer.create(NameThatSong.this, songList.get(currentSong).getTune());
                     aPlayer.start();
@@ -84,23 +101,32 @@ public class NameThatSong extends AppCompatActivity {
             {
                 //compare input vs song name
                 String userAnswer = answerText.getText().toString();
-                if (userAnswer.equals(null))
+                if (userAnswer.equals(null)) //no answer given
                 {
                     Toast.makeText(NameThatSong.this, "Please enter an answer", Toast.LENGTH_SHORT).show();
                 }
-                else
+                else //Answer was given
                 {
                     String actualAnswer = songList.get(currentSong).getSongName();
-                    if (userAnswer.equalsIgnoreCase(actualAnswer)) {
-                        Toast.makeText(NameThatSong.this, "Perfect!", Toast.LENGTH_SHORT).show();
+                    if (userAnswer.equalsIgnoreCase(actualAnswer)) //user guesses right
+                    {
                         submitBtn.setText("You Got It!!! \n\nTry again? A new song is waiting!");
                         aPlayer.stop();
 
-                        //TODO:get new song (update currentSong)
-                        currentSong = 0;
-                    } else {
+                        endTime = System.currentTimeMillis();
+                        timeTaken = endTime - startTime;
+                        Toast.makeText(NameThatSong.this, "Perfect! It took you " + timeTaken, Toast.LENGTH_SHORT).show();
+                        //TODO: convert to readable format. Check previous code for ready-made converter
+
+                        currentSong = rand.nextInt(songList.size()); //get random current song
+                        songPlayed = false; //reset song played
+                    }
+                    else //wrong guess
+                    {
                         Toast.makeText(NameThatSong.this, "BOOOOOOO", Toast.LENGTH_SHORT).show();
-                        submitBtn.setText("That's not right. It's a song by ....");
+
+                        /*give Hints*/
+                        submitBtn.setText("That's not right. It's a song by " + songList.get(currentSong).getArtist());
                     }
                 }
 
@@ -120,16 +146,29 @@ public class NameThatSong extends AppCompatActivity {
 //        Song song1 = new Song("In The End, Linkin Park,");
         List<String[]> dataFromFile = ReadCSV();
 
+        List<Song> songList = new ArrayList<>(); //holds song objects
+        /*loop through data and creates objects*/
         for (int i = 0; i < dataFromFile.size(); i++)
         {
             Log.e("Something", Arrays.toString(dataFromFile.get(i)));
-            Song aSong = new Song();
+
+            int tuneID = getResources().getIdentifier(dataFromFile.get(i)[2], "raw", getPackageName()); //get tune ID
+            int artID = getResources().getIdentifier(dataFromFile.get(i)[3], "drawable", getPackageName()); //get art ID
+            Song aSong = new Song
+                (
+                    dataFromFile.get(i)[0], //song name
+                    dataFromFile.get(i)[1], //artist name
+                    tuneID, //tune
+                    artID, //cover art
+                    dataFromFile.get(i)[4] //hint
+                );
+            songList.add(aSong);
         }
 
-        List<Song> songList = new ArrayList<>();
-        songList.add(song1);
         return songList;
     }
+
+    /*get Data from CSV file*/
     private List<String[]> ReadCSV()
     {
         List<String[]> dataList = new ArrayList<>();
