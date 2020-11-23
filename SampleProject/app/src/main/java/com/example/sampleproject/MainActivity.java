d88adbd1dc879a692ef1e814018f664288af6762
Talebi-T9630 <66806941+Talebi-T9630@users.noreply.github.com>
2020-11-22 19:17:16 -0800

    Main activity and grid view

--- SampleProject/app/src/main/java/com/example/sampleproject/MainActivity.java
+++ SampleProject/app/src/main/java/com/example/sampleproject/MainActivity.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 public class MainActivity extends AppCompatActivity
 {
-
-    Button startGameBtn; //declare button object
+    ImageView img;
+    Animation rotateAnim;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,21 +27,35 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startGameBtn = findViewById(R.id.startButton); //hook button object to preferred button
-        startGameBtn.setText("Level List");
-        /*give the button an onClickListener*/
-        startGameBtn.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                startActivity(new Intent(MainActivity.this, LevelList.class));
-            }
-        }); //end setOnClickListener of startGameBtn
+        editText=findViewById(R.id.userName);
+        img=findViewById(R.id.playButton);
+        img.setClickable(true);
+        rotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        final Intent intent=new Intent(MainActivity.this,LevelList.class);
 
-        //TODO: starts DB implementation
-        //TODO: get username
-            //TODO: check if username already exists in DB and act accordingly
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                img.startAnimation(rotateAnim);
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                    startActivity(intent);
+                }catch(Exception ex){
+                    Log.e("MainActivity", "Animation time out ");
+                }
+
+            }
+        });
+
+        Bundle bundle=new Bundle();
+        bundle.getString("UserName",editText.getText().toString());
+        intent.putExtras(bundle);
+
+
+
+
+
     }
 
     //return home on back pressed
@@ -45,4 +66,12 @@ public class MainActivity extends AppCompatActivity
         a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(a);
     }
+
+//    public void imageButton(View view){
+//        view=findViewById(R.id.playButton);
+//       Animation rotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+//       view.getRotation();
+//        startActivity(new Intent(MainActivity.this,LevelList.class));
+//
+//    }
 }
