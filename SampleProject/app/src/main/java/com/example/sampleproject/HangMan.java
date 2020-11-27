5aab42a60472e37a1725f7424e922da943375c24
Talebi-T9630 <66806941+Talebi-T9630@users.noreply.github.com>
2020-11-26 23:17:32 -0800

    hang man

--- SampleProject/app/src/main/java/com/example/sampleproject/HangMan.java
+++ SampleProject/app/src/main/java/com/example/sampleproject/HangMan.java
@@ -3,7 +3,9 @@ package com.example.sampleproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +18,12 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.sampleproject.Models.TapManModel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,40 +37,75 @@ public class HangMan extends AppCompatActivity {
             "DRIVE", "PARTY", "EAT", "DRINK", "CLEAN", "STUDY", "DIE"};
     final String[] words2020 = new String[]{"QUARANTINE", "COVID", "CORONA", "WWIII", "BLACK LIVES MATTER"};
 
-    String[] button=new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U"
-                                  ,"V","W","X","Y","Z"};
+    String[] button = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U"
+            , "V", "W", "X", "Y", "Z"};
 
     List<Integer> hangManPics = new ArrayList<>(Arrays.asList(R.drawable.hangman_lvl0,
-            R.drawable.hangman_lvl1,R.drawable.hangman_lvl2,R.drawable.hangman_lvl3,
-            R.drawable.hangman_lvl4,R.drawable.hangman_lvl5,R.drawable.hangman_lvl6));
-
+            R.drawable.hangman_lvl1, R.drawable.hangman_lvl2, R.drawable.hangman_lvl3,
+            R.drawable.hangman_lvl4, R.drawable.hangman_lvl5, R.drawable.hangman_lvl6));
 
 
     String wordChosen;
     ImageView hangManImg;
     TextView dashBox;
     TextView msgBox;
-    String dash="";
+    String dash = "";
     int tryCounter = 0; //amount of times the user guessed
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hang_man);
 
+        createDB();
+        createTables();
+
+        List<String[]> animals = ReadCSV("animals");
+
+        for(int i=1;i<animals.size();i++){ //we start with one cause we don't need the column titles
+            int id=Integer.parseInt(animals.get(i)[0]);
+            String word=animals.get(i)[1];
+            String hint=animals.get(i)[2];
+
+            addWordsToAnimals(id,word,hint);
+
+        }
+        List<String[]> activities = ReadCSV("activity");
+
+        for(int i=1;i<activities.size();i++){ //we start with one cause we don't need the column titles
+            int id=Integer.parseInt(activities.get(i)[0]);
+            String word=activities.get(i)[1];
+            String hint=activities.get(i)[2];
+
+            addWordsToActivities(id,word,hint);
+
+        }
+        List<String[]> twenty_twenty = ReadCSV("activity");
+
+        for(int i=1;i<twenty_twenty.size();i++){ //we start with one cause we don't need the column titles
+            int id=Integer.parseInt(twenty_twenty.get(i)[0]);
+            String word=twenty_twenty.get(i)[1];
+            String hint=twenty_twenty.get(i)[2];
+
+            addWordsToActivities(id,word,hint);
+
+        }
+
+
 
         hangManImg = findViewById(R.id.hangManImg);
         category = findViewById(R.id.categorySpin);
-        dashBox=findViewById(R.id.dashBox);
-        msgBox=findViewById(R.id.msgBox);
+        dashBox = findViewById(R.id.dashBox);
+        msgBox = findViewById(R.id.msgBox);
 
         category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-              //  Toast.makeText(HangMan.this, "Kar kon", Toast.LENGTH_SHORT).show();
+                //  Toast.makeText(HangMan.this, "Kar kon", Toast.LENGTH_SHORT).show();
                 //reset game when changed
-                dash =""; //reset dash board
+                dash = ""; //reset dash board
                 //reset picture
                 hangManImg.setImageResource(R.drawable.hangman_lvl0);
                 //reset tries
@@ -72,28 +115,28 @@ public class HangMan extends AppCompatActivity {
                 letterColorBack(button);
                 switch (position) {
                     case 0:
-                        wordChosen="";
+                        wordChosen = "";
                         Toast.makeText(HangMan.this, "Please choose a category", Toast.LENGTH_SHORT).show();
                         break;
                     case 1:
-                       wordChosen= getRandom(wordsAnimal);
+                        wordChosen = getRandom(wordsAnimal);
                         break;
                     case 2:
-                        wordChosen=getRandom(wordsActivities);
+                        wordChosen = getRandom(wordsActivities);
                         break;
                     case 3:
-                        wordChosen=getRandom(words2020);
+                        wordChosen = getRandom(words2020);
                         break;
                 }
 
-                for(int i=0;i<wordChosen.length();i++){
-                    dash=dash+"_";
+                for (int i = 0; i < wordChosen.length(); i++) {
+                    dash = dash + "_";
 
 
                 }
                 String displayText = interlace(dash);
                 dashBox.setText(displayText);
-               // msgBox.setText(wordChosen);
+                // msgBox.setText(wordChosen);
 
             }
 
@@ -111,21 +154,19 @@ public class HangMan extends AppCompatActivity {
         return array[rnd];
     }
 
-    public static String interlace(String aString)
-    {
+    public static String interlace(String aString) {
         String interlaced = " ";
-        for (int i = 0; i < aString.length(); i++)
-        {
+        for (int i = 0; i < aString.length(); i++) {
             interlaced += aString.charAt(i) + " ";
         }
         return interlaced;
     }
 
     public void touchLetter(View view) {
-        Button btn=(Button)view;
-        String dash2="";
-       // Toast.makeText(this, "Letter entered "+btn.getText(), Toast.LENGTH_LONG).show();
-        char letterChosen=btn.getText().charAt(0);
+        Button btn = (Button) view;
+        String dash2 = "";
+        // Toast.makeText(this, "Letter entered "+btn.getText(), Toast.LENGTH_LONG).show();
+        char letterChosen = btn.getText().charAt(0);
         boolean correct = false; //initially false
 
         //only run code if there are still tries left
@@ -133,8 +174,7 @@ public class HangMan extends AppCompatActivity {
         {
             msgBox.setText("Game over Bruh!");
 
-        }
-        else //still have tries
+        } else //still have tries
         {
             for (int i = 0; i < wordChosen.length(); i++) {
                 if (letterChosen == wordChosen.charAt(i)) {
@@ -149,14 +189,12 @@ public class HangMan extends AppCompatActivity {
             }
 
             //if user guessed wrong
-            if (correct == false)
-            {
+            if (correct == false) {
                 tryCounter++;
                 int imgIs = hangManPics.get(tryCounter);
                 hangManImg.setImageResource(imgIs);
-                Toast.makeText(this, "Wrong letter" , Toast.LENGTH_SHORT).show();
-            }
-            else //user guessed right
+                Toast.makeText(this, "Wrong letter", Toast.LENGTH_SHORT).show();
+            } else //user guessed right
             {
                 if (dash2.indexOf("_") == -1) //no _ means we've won!
                 {
@@ -173,26 +211,169 @@ public class HangMan extends AppCompatActivity {
     }
 
 
-    public void letterColorBack(String[] letters){
+    public void letterColorBack(String[] letters) {
         Button[] letterButton = new Button[26];
 
-        for(int i=0;i<letters.length;i++) {
+        for (int i = 0; i < letters.length; i++) {
             String buttonNumber = "button" + letters[i];
-            int buttonId=getResources().getIdentifier(buttonNumber,"id",getPackageName());
-            Log.e("TAG", "button "+buttonId);
-            letterButton[i]=findViewById(buttonId);
+            int buttonId = getResources().getIdentifier(buttonNumber, "id", getPackageName());
+            Log.e("TAG", "button " + buttonId);
+            letterButton[i] = findViewById(buttonId);
             letterButton[i].setBackgroundColor(getResources().getColor(R.color.buttonHangMan));
 
         }
 
 
-   }
-
-    //returns to Level List when back button is pressed
-    @Override
-    public void onBackPressed() {
-
-        startActivity(new Intent(this,LevelGrid.class));
     }
 
-}
+
+    public void createDB() {
+        try {
+            db = db.openOrCreateDatabase("HangMan.db", null);
+
+        } catch (Exception ex) {
+            Log.e("Tappy DB", ex.getMessage());
+        }
+
+    }
+
+    private void createTables(){
+        try {
+            String setPRAGMAForeignKeyOn = "PRAGMA foreign_keys = ON;";
+            String dropAnimalsTable = "DROP TABLE IF EXISTS " + "animals;";
+            String dropActivitiesTable = "DROP TABLE IF EXISTS " + "activities;";
+            String drop2020Table = "DROP TABLE IF EXISTS " + "twenty_twenty;";
+
+            String createAnimalsTable = "CREATE TABLE animals " + "(ID INT  PRIMARY KEY, " +
+                    "Word TEXT, Hint TEXT);";
+            String createActivitiesTable = "CREATE TABLE activities " + "(ID INT  PRIMARY KEY, " +
+                    "Word TEXT, Hint TEXT);";
+
+            String create2020Table = "CREATE TABLE twenty_twenty " + "(ID INT  PRIMARY KEY, " +
+                    "Word TEXT, Hint TEXT);";
+
+            db.execSQL(setPRAGMAForeignKeyOn);
+            db.execSQL(dropAnimalsTable);
+            db.execSQL(dropActivitiesTable);
+            db.execSQL(drop2020Table);
+
+            db.execSQL(createAnimalsTable);
+            db.execSQL(createActivitiesTable);
+            db.execSQL(create2020Table);
+        }catch (Exception ex){
+
+        }
+    }
+
+            //returns to Level List when back button is pressed
+//    @Override
+//    public void onBackPressed(){
+//
+//        startActivity(new Intent(this,LevelGrid.class));
+//    }
+//
+//
+//
+//        }catch(Exception ex){
+//            Log.e("DB demo","Error msg"+ex.getMessage());
+//        }
+//
+//    }
+
+            public void addWordsToAnimals (Integer id, String Word, String Hint){
+                long result;
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("ID", id);
+                contentValues.put("Word", Word);
+                contentValues.put("Hint", Hint);
+
+                result = db.insert("animals", null, contentValues);// by adding the null we are specifying that no column can be null
+                if (result != -1) {
+                    Log.e("DB demo", "It is done");
+                } else {
+                    Log.e("DB demo", " Error inserting rec");
+                }
+            }
+
+            public void addWordsToActivities (Integer id, String Word, String Hint){
+                long result;
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("ID", id);
+                contentValues.put("Word", Word);
+                contentValues.put("Hint", Hint);
+
+                result = db.insert("activitytables", null, contentValues);// by adding the null we are specifying that no column can be null
+                if (result != -1) {
+                    Log.e("DB demo", "It is done");
+                } else {
+                    Log.e("DB demo", " Error inserting rec");
+                }
+            }
+
+            public void addWordsTo2020 (Integer id, String Word, String Hint){
+                long result;
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("ID", id);
+                contentValues.put("Word", Word);
+                contentValues.put("Hint", Hint);
+
+                result = db.insert("twenty_twenty", null, contentValues);// by adding the null we are specifying that no column can be null
+                if (result != -1) {
+                    Log.e("DB demo", "It is done");
+                } else {
+                    Log.e("DB demo", " Error inserting rec");
+                }
+            }
+
+
+            private List<String[]> ReadCSV (String fileName){
+                List<String[]> arr = new ArrayList<>();
+
+                //populate the list
+                InputStream inputStream = null;
+                switch (fileName) {
+                    case "animals":
+                        inputStream = getResources().openRawResource(R.raw.animals);
+                        break;
+                    case "activity":
+                        inputStream = getResources().openRawResource(R.raw.activitytables);
+                        break;
+                    case "2020":
+                        inputStream = getResources().openRawResource(R.raw.twenty_twenty);
+                        break;
+                }
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                try {
+                    String csvLine;
+                    while ((csvLine = reader.readLine()) != null) {
+
+                        String[] eachWord = csvLine.split(",");
+                        if (!eachWord[0].equals("ID")) {
+                            arr.add(eachWord);
+                        }
+
+
+                    }
+
+                } catch (IOException ex) {
+                    throw new RuntimeException("Error " + ex);
+
+
+                } finally {
+                    try {
+                        inputStream.close();
+
+                    } catch (IOException ex) {
+                        throw new RuntimeException("Error" + ex);
+
+                    }
+                }
+
+                return arr;
+            }
+        }
+    
+
+
+
